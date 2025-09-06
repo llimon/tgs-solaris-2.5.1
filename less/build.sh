c23bf495465b0484ff23533d24150853ba28cf19
Luis E Limon <9660709+llimon@users.noreply.github.com>
2025-09-06 14:21:34 -0700

    Add less and libsnprintf (with sources)

--- less/build.sh
+++ less/build.sh
@@ -7,7 +7,7 @@
 # Check the following 4 variables before running the script
 topdir=less
 version=530
-pkgver=1
+pkgver=2
 source[0]=http://www.greenwoodsoftware.com/less/$topdir-$version.tar.gz
 # If there are no patches, simply comment this
 #patch[0]=
@@ -18,6 +18,7 @@ source[0]=http://www.greenwoodsoftware.com/less/$topdir-$version.tar.gz
 # Global settings
 export CPPFLAGS="-I$prefix/include"
 export LDFLAGS="-L$prefix/lib -R$prefix/lib"
+export LIBS="-lw"
 configure_args+=(--with-editor=/bin/vi)
 
 reg prep
--- libsnprintf/build.sh
+++ libsnprintf/build.sh
@@ -2,19 +2,16 @@
 # This is a buildpkg build.sh script
 # build.sh helper functions
 . ${BUILDPKG_SCRIPTS}/build.sh.functions
-#
-###########################################################
+# ###########################################################
 # Check the following 4 variables before running the script
-topdir=snprintf
-version=2.2
+topdir=libsnprintf
+version=2.2.1
 version_major="${version%.*}"
 
 pkgver=1
 
-# https://ftp.deu.edu.tr/pub/Solaris/sunfreeware/SOURCES/snprintf-2.2.tar.gz
 source[0]=https://ftp.deu.edu.tr/pub/Solaris/sunfreeware/SOURCES/${topdir}-${version}.tar.gz
 # If there are no patches, simply comment this
-patch[0]=autoconf-fix.patch
 
 # Source function library
 . ${BUILDPKG_SCRIPTS}/buildpkg.functions
@@ -23,8 +20,7 @@ patch[0]=autoconf-fix.patch
 
 # Global settings
 export CFLAGS="-std=gnu99" 
-export LDFLAGS="-lgcc_s"
-topsrcdir="${topdir}_${version}/with_autoconf"
+export LIBS="-lgcc_s"
 #configure_args+=()
 
 reg prep
@@ -51,7 +47,7 @@ install()
 {
     DESTDIR=${stagedir}
     generic_install DESTDIR
-    doc README INSTALL LICENSE.txt
+    doc ChangeLog NEWS README INSTALL LICENSE.txt
 }
 
 reg pack
