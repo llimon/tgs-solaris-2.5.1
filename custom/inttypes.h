1dd90b95ff3fab649554de6f9c13ccfc54e43183
Luis E Limon <9660709+llimon@users.noreply.github.com>
2025-10-05 01:10:41 -0700

    add printf macros

--- custom/inttypes.h
+++ custom/inttypes.h
@@ -1,6 +1,25 @@
 #ifndef _SPARC32_INTTYPE_H
 #define _SPARC32_INTTYPE_H
 
+
+#if defined(__sun__) && defined(__sparc__)
+// For 32-bit SPARC, like Sun-4m/Solaris 2.5.1
 #include <stdint.h>
+		       //
+
+#define PRIdPTR "ld"
+#define PRIuPTR "lu"
+
+#elif defined(__x86_64__) || defined(__aarch64__)
+// For 64-bit systems
+#define PRIdPTR "ld"
+#define PRIuPTR "lu"
+
+#else
+// Default for older 32-bit systems
+#define PRIdPTR "d"
+#define PRIuPTR "u"
+
+#endif
 
 #endif /* _SPARC32_INTTYPE_H */
